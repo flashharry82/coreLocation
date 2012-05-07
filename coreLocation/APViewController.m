894a4910828bc501fc3b31e827573d8ece117b86
Alan Stirling <alan@johnstonnorth.com>
2012-05-07 15:34:30 +0100

    added some commments and simplified code a bit

--- coreLocation/APViewController.m
+++ coreLocation/APViewController.m
@@ -18,6 +18,8 @@
 @synthesize currentLocation = _currentLocation;
 @synthesize gpsOutput = _gpsOutput;
 
+
+// this method fetches the current location and sets local variable to it, it also checks for accuracy which if is less than 100m stops updating the location
 -(void) locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
     self.currentLocation = newLocation;
@@ -27,29 +29,26 @@
     }
 }
 
+
+// this method runs if there is an error when getting location information
 -(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
-    if(error.code == kCLErrorDenied) {
-        [_locationManager stopUpdatingLocation];
-    } else if(error.code == kCLErrorLocationUnknown) {
-        // retry
-    } else {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error retrieving location"
-                                                        message:[error description]
-                                                       delegate:nil
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
-        [alert show];
-    }
+    [_locationManager stopUpdatingLocation];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error retrieving location"
+                                                    message:[error description]
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 - (void)viewDidLoad
 {
-    self.locationManager = [CLLocationManager new];
-    self.locationManager.delegate = self;
-    [self.locationManager startUpdatingLocation];
+    self.locationManager = [CLLocationManager new]; // create new instance of CLLocationManager
+    self.locationManager.delegate = self; // set delegate of CLLocationManager instance to current class
+    [self.locationManager startUpdatingLocation]; // run startUpdatingLocation method
     
-    self.gpsOutput.text = [NSString stringWithFormat:@"%@", self.locationManager.location];
+    self.gpsOutput.text = [NSString stringWithFormat:@"%@", self.locationManager.location]; // set label text to location information
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
@@ -64,9 +63,5 @@
     [self.locationManager stopUpdatingLocation];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
 
 @end
